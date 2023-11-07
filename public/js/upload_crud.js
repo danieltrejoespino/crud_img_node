7855c6e309169233871fb6fb71dc8b3df2623d2b
Daniel Trejo <daniel.trejo.espino@gmail.com>
2023-11-07 11:36:24 -0600

    work with uploads

--- public/js/upload_crud.js
+++ public/js/upload_crud.js
@@ -1,13 +1,41 @@
 const btn_image=document.querySelector('#btn_image')
-const imagenInput = document.getElementById('formFileLg')
+const uploadInput = document.getElementById('formFileLg')
+
+
 
 btn_image.addEventListener('click', function () {
-  const imagen = imagenInput.files[0];
-  if (imagen) {
-    const formData = new FormData();
-    formData.append('imagen', imagen);
+  if (valida()== true) {
+    upload()
+  }
+  //  else {
+  //   show_alert(2,'Agrega una archivo a subir')
+  // }  
+});
 
-    fetch('/uploadImage', {
+function valida(){
+  const allowedTypes = ['image/png','image/jpeg', 'application/pdf', 'text/plain', 'audio/mpeg'];
+  const upload = uploadInput.files[0];
+
+  if (!upload) {
+    show_alert(2, 'Agrega un archivo para subir');
+    return false;
+  }  
+  if (!allowedTypes.includes(upload.type)) {
+    console.log(upload.type);
+    show_alert(2, 'Formato de archivo no valido. Se admiten tipos como JPEG, PDF, texto y MP3.');
+    return false;
+  }  
+  return true;
+}
+
+function upload() { 
+  btn_image.disabled=true
+  const upload = uploadInput.files[0];   
+  const ruta='/uploadImage'
+
+  const formData = new FormData();
+  formData.append('imagen', upload);
+    fetch(ruta, {
       method: 'POST',
       body: formData,
     })
@@ -18,10 +46,11 @@ btn_image.addEventListener('click', function () {
       }else{
         show_alert(2,'Error al subir la imagen')        
       }
-    }).catch((err) => {
+    })
+    .then((result) => {
+      btn_image.disabled=false
+    })
+    .catch((err) => {
       console.error('Error al subir la imagen:', error);
-    });
-  }else{
-    show_alert(2,'Agrega una imagen para continuar')
-  }
-});
+    });  
+}
